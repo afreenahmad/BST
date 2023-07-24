41a0236b39386f637985955ce1c2ea94237a85cf
afreenahmad <afreenahmad01@gmail.com>
2023-07-23 20:59:35 -0400

    delete method

--- src/BST/BST.java
+++ src/BST/BST.java
@@ -5,7 +5,7 @@ import java.util.List;
 
 public class BST {
 
-    private static class Node {
+    public static class Node {
         Album data;
         Node left;
         Node right;
@@ -58,5 +58,78 @@ public class BST {
             result.add(node.data);
             inOrderTraversal(node.right, result);
         }
+    }
+
+    public Node delete(Album data) {
+        if (root == null) {
+            throw new IllegalArgumentException("The BST is empty.");
+        }
+
+        return deleteRec(root, null, data);
+    }
+
+    private Node deleteRec(Node current, Node parent, Album data) {
+        if (current == null) {
+            throw new IllegalArgumentException("does not exist.");
+        }
+
+        int comparison = data.compareTo(current.data);
+
+        if (comparison < 0) {
+            current.left = deleteRec(current.left, current, data);
+        } else if (comparison > 0) {
+            current.right = deleteRec(current.right, current, data);
+        } else {
+            // Case 1: Node to be deleted is a leaf node
+            if (current.left == null && current.right == null) {
+
+                if (parent == null) {
+                    root = null;
+                } else {
+
+                    if (parent.left == current) {
+                        parent.left = null;
+                    } else {
+                        parent.right = null;
+                    }
+                }
+            }
+            // Case 2: Node to be deleted has only one child
+            else if (current.left == null) {
+                if (parent == null) {
+                    root = current.right;
+                } else {
+                    if (parent.left == current) {
+                        parent.left = current.right;
+                    } else {
+                        parent.right = current.right;
+                    }
+                }
+            } else if (current.right == null) {
+                if (parent == null) {
+                    root = current.left;
+                } else {
+                    if (parent.left == current) {
+                        parent.left = current.left;
+                    } else {
+                        parent.right = current.left;
+                    }
+                }
+            }
+            // Case 3: Node to be deleted has two children
+            else {
+                Node successor = findMin(current.right);
+                current.data = successor.data;
+                current.right = deleteRec(current.right, current, successor.data);
+            }
+        }
+        return current;
+    }
+
+    private Node findMin(Node node) {
+        while (node.left != null) {
+            node = node.left;
+        }
+        return node;
     }
 }
--- src/BST/BinarySearchTreeTest.java
+++ src/BST/BinarySearchTreeTest.java
@@ -32,4 +32,31 @@ class BinarySearchTreeTest {
         assertEquals(album3, inOrderTraversal.get(2));
     }
 
+    @Test
+    public void testDelete() {
+        BST bst = new BST();
+
+        Album album1 = new Album(1, Arrays.asList("Artist1"), "Album 1", 10);
+        Album album2 = new Album(2, Arrays.asList("Artist2", "Artist3"), "Album 2", 15);
+        Album album3 = new Album(3, Arrays.asList("Artist4"), "Album 3", 5);
+
+        bst.insert(album1);
+        bst.insert(album2);
+        bst.insert(album3);
+
+        assertEquals(album1, bst.delete(album1).data);
+
+        // Verify that album1 is deleted from the BST
+        List<Album> inOrderTraversalAfterDeletion = bst.getInOrderTraversal();
+        assertEquals(album2, inOrderTraversalAfterDeletion.get(0));
+        assertEquals(album3, inOrderTraversalAfterDeletion.get(1));
+
+        // Verify that attempting to delete album1 again throws an IllegalArgumentException
+        assertThrows(IllegalArgumentException.class, () -> bst.delete(album1));
+
+        // Verify that attempting to delete a non-existent album throws an IllegalArgumentException
+        Album nonExistentAlbum = new Album(4, Arrays.asList("Artist5"), "Album 4", 8);
+        assertThrows(IllegalArgumentException.class, () -> bst.delete(nonExistentAlbum));
+    }
+
 }
